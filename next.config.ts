50cf73d22f101750aa1ee97e20a3cba8d4776bbc
TheKany <skarnjs06@naver.com>
2025-02-17 16:30:54 +0900

    pwa 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   compiler: {
     styledComponents: true,
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig,
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
