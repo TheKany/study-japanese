78fbcc79f10e6008e768a2fe2ffc80fa3e84201f
TheKany <skarnjs06@naver.com>
2025-02-27 21:53:45 +0900

    버튼 글자 color 추가

--- src/components/ButtonBox/index.tsx
+++ src/components/ButtonBox/index.tsx
@@ -12,6 +12,7 @@ export const ButtonBox = styled.button<Props>`
   height: ${({ height }) => height || "50px"};
   border-radius: 8px;
   background-color: #ffd20a;
+  color: #121212;
   margin: 8px 0;
   font-size: 16px;
   font-weight: 700;
